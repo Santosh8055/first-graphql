b8d3216385ceae2d2e90c8773bf0476d708fa862
Santosh Pasupunuri <skpadmin@hyperiongrp.com>
2021-12-03 10:32:17 +0530

    added graphql decorators in input files and import graphql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path';
 import { HobbyModule } from './hobby/hobby.module';
 import { PersonModule } from './person/person.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/first-graphql'),
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: true,
+      debug: false,
+    }),
     PersonModule,
     HobbyModule,
   ],
