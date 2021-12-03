e4e1029cd59a9d5e19b4e2a99a343b4f8bd1c16d
Santosh Pasupunuri <skpadmin@hyperiongrp.com>
2021-12-03 09:57:17 +0530

    added person and hobby modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { HobbyModule } from './hobby/hobby.module';
+import { PersonModule } from './person/person.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/first-graphql')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/first-graphql'),
+    PersonModule,
+    HobbyModule,
+  ],
 })
 export class AppModule {}
