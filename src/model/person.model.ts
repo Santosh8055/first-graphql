77e8cde616ac88e528e337ef9139a65d924c8faf
Santosh Pasupunuri <skpadmin@hyperiongrp.com>
2021-12-03 10:14:58 +0530

    changes to person model

--- src/model/person.model.ts
+++ src/model/person.model.ts
@@ -1,7 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Hobby } from './hobby.model';
 import { Document, Schema as MongooseSchema } from 'mongoose';
-
 @Schema()
 export class Person {
   _id: MongooseSchema.Types.ObjectId;
@@ -9,8 +8,8 @@ export class Person {
   @Prop()
   name: string;
 
-  @Prop()
-  hobbies: Hobby[];
+  @Prop({ type: [MongooseSchema.Types.ObjectId], ref: Hobby.name })
+  hobbies: MongooseSchema.Types.ObjectId[];
 }
 
 export type PersonDocument = Person & Document;
--- src/person/person.inputs.ts
+++ src/person/person.inputs.ts
@@ -1,19 +1,18 @@
 import { Schema as MongooseSchema } from 'mongoose';
-import { Hobby } from 'src/model/hobby.model';
 
 export class CreatePersonInput {
   name: string;
-  hobbies: Hobby[];
+  hobbies: MongooseSchema.Types.ObjectId[];
 }
 
 export class ListPersonInput {
   _id?: MongooseSchema.Types.ObjectId;
   name?: string;
-  hobbies?: Hobby[];
+  hobbies?: MongooseSchema.Types.ObjectId[];
 }
 
 export class UpdatePersonInput {
   _id: MongooseSchema.Types.ObjectId;
   name?: string;
-  hobbies?: Hobby[];
+  hobbies?: MongooseSchema.Types.ObjectId[];
 }
