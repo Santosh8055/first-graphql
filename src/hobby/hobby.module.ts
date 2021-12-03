f07e72fe0e897180a86f24d6d4eb7e26b75ba513
Santosh Pasupunuri <skpadmin@hyperiongrp.com>
2021-12-03 10:28:13 +0530

    added resolvers

--- src/hobby/hobby.module.ts
+++ src/hobby/hobby.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Hobby, HobbySchema } from './hobby.model';
 import { HobbyService } from './hobby.service';
+import { HobbyResolver } from './hobby.resolver';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Hobby.name, schema: HobbySchema }]),
   ],
-  providers: [HobbyService],
+  providers: [HobbyService, HobbyResolver],
 })
 export class HobbyModule {}
--- src/person/person.module.ts
+++ src/person/person.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Person, PersonSchema } from 'src/person/person.model';
 import { PersonService } from './person.service';
+import { PersonResolver } from './person.resolver';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Person.name, schema: PersonSchema }]),
   ],
-  providers: [PersonService],
+  providers: [PersonService, PersonResolver],
 })
 export class PersonModule {}
